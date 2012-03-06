f36fbba95d65cc5ad3335a5f81647162ea902df2
Evelyn Kislig <evelynorama@yahoo.com>
2012-03-05 21:50:09 -0800

    added new to do list view

---
+++ MyFirstMVCApp/Controllers/TodolistController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class TodolistController : Controller
+    {
+        //
+        // GET: /Todolist/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
